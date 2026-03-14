961575c24b30bced9eed0a8a7f8356137c87e00f
AlexKo71 <alekko-sp@yandex.ru>
2026-03-14 14:55:11 +0300

    final version

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const menu = document.querySelector(".menu-btn");
+const menuSpan = document.querySelector(".menu-span");
+console.log(menu);
 
+menu.addEventListener("click", () => {
+  menuSpan.classList.toggle("hide");
+});
